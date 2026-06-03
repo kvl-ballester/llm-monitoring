605fe7603a2d507bf0c6d61ad3f4defc9e013203
kvl.ballester <82665860+kvl-ballester@users.noreply.github.com>
2026-06-03 20:46:06 +0000

    add persistance

--- assistant.py
+++ assistant.py
@@ -7,6 +7,7 @@ import os
 from ingest import load_faq_data, build_index
 #from rag_helper import RAGBase
 from rag_helper import RAGWithMetrics
+from db_save import save_conversation
 
 def create_assistant():
     load_dotenv()
@@ -31,3 +32,5 @@ if __name__ == "__main__":
 
     answer = assistant.rag(query)
     print(answer)
+    
+    save_conversation(assistant.last_call, query, "llm-zoomcamp")
