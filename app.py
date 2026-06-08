38d10343f21da07e3218f697ecb211f0df6443fc
kvl.ballester <82665860+kvl-ballester@users.noreply.github.com>
2026-06-08 08:37:56 +0000

    add feedback

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from assistant import create_assistant
 from db_save import save_conversation
+from db_feedback import save_feedback
 
 assistant = create_assistant()
 
@@ -22,3 +23,16 @@ if st.button("Ask"):
 
         conversation_id = save_conversation(record, user_input, "llm-zoomcamp")
         st.session_state.conversation_id = conversation_id
+        
+col1, col2 = st.columns(2)
+with col1:
+    if st.button("+1"):
+        cid = st.session_state.conversation_id
+        save_feedback(cid, "user", score=1)
+        st.write("Thanks!")
+
+with col2:
+    if st.button("-1"):
+        cid = st.session_state.conversation_id
+        save_feedback(cid, "user", score=-1)
+        st.write("Thanks for the feedback!")
